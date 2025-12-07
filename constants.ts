c945a0df13f7daf17831d020b20555c107958e5f
Raid-r <117030438+Raid-r@users.noreply.github.com>
2025-12-07 14:37:31 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 // Replace these with your actual Supabase project details
-export const SUPABASE_URL = "https://YOUR-PROJECT-ID.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+export const SUPABASE_URL = "https://wljqwhayegsukneznrau.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsanF3aGF5ZWdzdWtuZXpucmF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NDAzNDgsImV4cCI6MjA3NDAxNjM0OH0.DqGBdy8rikmVwTLhEhJYUT-NNfolyUOS-XUfWHZn3Io";
 export const SUPABASE_BUCKET = "digits";
 
 export const CANVAS_SIZE = 280;
